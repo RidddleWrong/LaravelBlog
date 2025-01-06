65ad0905f62978dae746613c5349137a24dfb4a6
RidddleWrong <targa1910@ukr.net>
2025-01-06 21:33:03 +0200

    first commit

---
+++ app/Observers/CommentObserver.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Observers;
+
+use App\Jobs\SendEmailJob;
+use App\Models\Comment;
+
+class CommentObserver
+{
+    /**
+     * Handle the Comment "created" event.
+     *
+     * @param Comment $comment
+     * @return void
+     */
+    public function created(Comment $comment)
+    {
+        $post = $comment->post;
+        $email = $post->author->email;
+        $text = 'New comment was added to your post <a href="' .route('post.show', $post->id). '">' . e($post->title) . '</a>';
+        $subject = 'New comment was added to your post';
+
+        SendEmailJob::dispatch($email, $text, $subject);
+    }
+}
